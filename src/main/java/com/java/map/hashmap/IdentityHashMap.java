a378ca86045589480371337ce6f9e20c081c3aa1
sarita <saritapanda1996@gmail.com>
2020-01-13 21:22:09 +0530

    renamed file name

--- src/main/java/com/java/map/hashmap/IdentityHashMap.java
+++ src/main/java/com/java/map/hashmap/IdentityHashMap.java
@@ -4,13 +4,13 @@ import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.Map;
 
-public class IdentiryHashMap {
+public class IdentityHashMap {
     public static void main(String[] args) {
         Map<Integer,String> integerStringHashMap=new HashMap<>();
         integerStringHashMap.put(1,"aloka");
         integerStringHashMap.put(1,"nishan");
         System.out.println(integerStringHashMap);
-        Map<Integer,String> integerStringIdentityHashMap=new IdentityHashMap<>();
+        Map<Integer,String> integerStringIdentityHashMap=new java.util.IdentityHashMap<>();
         integerStringIdentityHashMap.put(1,"aloka");
         integerStringIdentityHashMap.put(2,"nishan");
         System.out.println(integerStringIdentityHashMap );
